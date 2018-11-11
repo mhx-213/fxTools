831cd89dff50e7fb7ec09b7f2a07099cd8d3539a
Airam-12 <Airam-12@users.noreply.github.com>
2018-11-11 15:18:47 +0000

    Added Create Merge in OUT tool

--- toolsAiram.py
+++ toolsAiram.py
@@ -9,15 +9,18 @@ def colores():
 		hou.ui.displayMessage("Como quieres que ponga un color si no seleccionas un nodo?")
 	else:	
 		for i in sel:
-			color=hou.ui.selectColor()
-			i.setColor(color)
+			try:
+				color=hou.ui.selectColor()
+				i.setColor(color)
+			except:
+				pass
 
 def generateOut():
 	try:
 		selected=hou.selectedNodes()
 		nodo=selected[0]
 		padre=nodo.parent()
-		panel=hou.ui.readInput("Nombre del OUT_", buttons=('Only OUT',"Create ROP", "Cancel"),close_choice=2)
+		panel=hou.ui.readInput("Nombre del OUT_", buttons=('Only OUT',"Create Obj. Merge", "Cancel"),close_choice=2)
 		#Create Null
 		if panel[0] == 0:
 			nul=padre.createNode("null", "OUT_" + panel[1])
@@ -29,17 +32,20 @@ def generateOut():
 			nul.setColor(color)
 		#Create ROP
 		elif panel[0]==1:
-			nul=padre.createNode("null", "OUT_" + panel[1])
-			nul.setInput(0,nodo)
+			objMerge=padre.createNode("object_merge", "MERGE_" + panel[1])
+			objMerge.parm("objpath1").set(str(nodo.path()))
 			posicionNodo=nodo.position()
-			nul.setPosition(posicionNodo)
-			nul.move([0,-1])
-			color=hou.Color([0,0.45,0.9])
-			nul.setColor(color)
-			nul.setSelected(True, clear_all_selected=True)
+			objMerge.setPosition(posicionNodo)
+			objMerge.move([0,-1])
+			color=hou.Color([0.68,0.4,1])
+			objMerge.setColor(color)
+			objMerge.setSelected(1)
+			objMerge.setDisplayFlag(1)
+			objMerge.setRenderFlag(1)
+
 		#Cancel
 		else:
-			print ("Nada")
+			pass
 	except:
 		hou.ui.displayMessage("Selecciona algun nodo anda")
 	
